c670c894649a3114e4a58c9670d228ef5c47d9be
EchoPr <arkasha.pronin.2005@gmail.com>
2023-09-25 15:30:08 +0300

    Added readme

--- nim_game.cpp
+++ nim_game.cpp
@@ -2,8 +2,15 @@
 #include <vector>
 #include <algorithm>
 #include <string>
+#include <random>
+#include <ctime>
 
 using namespace std;
+
+#define OK 100
+#define NOT_OK 101
+#define ERROR 102
+#define END_GAME 103
 
 
 struct GameField {
@@ -21,10 +28,11 @@ struct GameField {
 
     int take_chips(int row, int num) {
         if (row < 0 || row > 2 || field[row] < num)
-            return 1;
+            return NOT_OK;
 
         field[row] -= num;
-        return 0;
+        chips_amount -= num;
+        return OK;
     }
 
     int get_chip(int row) {
@@ -35,16 +43,14 @@ struct GameField {
 GameField game_field;
 
 bool game_stop() {
-    if (game_field.get_chip(0) + game_field.get_chip(1) + game_field.get_chip(2) == 0)
-        return 1;
-    return 0;
+    return game_field.chips_amount == 0;
 }
 
 void show_game_state() {
     cout << "Number of row" << string(39, ' ') << "Chips" << endl;;
     for (int i = 1; i < 4; i++) {
         int c = game_field.get_chip(i - 1);
-        cout << string(5, ' ') << i << string(18, ' ') << string(c, '*') << string(22 + 5 - c, ' ') << c << '\n';
+        cout << string(5, ' ') << i << string(21, ' ') << string(c, '*') << string(22 + 5 - c, ' ') << c << '\n';
     }
 }
 
@@ -56,7 +62,8 @@ void show_start_screen() {
 
     show_game_state();
 
-    cout << "Enter your turn in format <ROW, CHIPS> (For example,\n    2 3 - Take 3 chips from 2 row\n    Or enter 0 0 for exit)" << endl;
+    cout << "In your stroke enter your turn in format <ROW, CHIPS> (For example,\n    2 3 - Take 3 chips from 2 row\n    Or enter 0 0 for exit)" << endl;
+    cout << "There are 3 start conditions (First turn by: 1 - user; 2 - computer; 3 - take it random)" << endl;
 }
 
 int user_turn() {
@@ -73,14 +80,33 @@ int user_turn() {
         throw runtime_error("Invalide input data! Expected turn <row, num_of_chips>");
     }
 
-    if (row == 0 && num == 0) return -1;
+    if (row == 0 && num == 0) return END_GAME;
 
     return game_field.take_chips(row - 1, num);
 }
 
+int user_handler(){
+    int user_moved = 0;
+        while (!user_moved) {
+            switch (user_turn()) {
+            case END_GAME:
+                cout << "Game break!" << endl;;
+                return END_GAME;
+                break;
+            case NOT_OK:
+                cout << "Incorrect move!" << endl;;
+                break;
+            case OK:
+                user_moved = 1;
+                break;
+            }
+        }
+    return OK;
+}
+
 string pull_time(int row) {
     game_field.take_chips(row, 1);
-    return ("Computer move: " + char(row + 1 + 48)) + ' ' + '\n';
+    return ("Computer move: " + char(row + 49)) + ' ' + '\n';
 }
 
 int xor_heaps(vector<int> d) {
@@ -113,43 +139,56 @@ string comp_turn() {
     }
 }
 
-int main() {
-    system("chcp 1251");
+int choose_player(){
+    cout << "Choose your stroke: ";
+    int st;
 
+    while (1){
+        cin >> st;
+        if (st < 1 || st > 3){
+            cout << "Incorrect type. Try again\nChoose your stroke: " << endl;
+            continue;
+        }
+        if (st == 3){
+            srand(time(0));
+            return rand() % 2 + 1;
+        }
+
+        return st;    
+    }
+}
+
+int main() {
     show_start_screen();
 
+    int user_stroke = choose_player(); // 1 - first; 2 - second;
+    int stroke = 0;
+
+
     while (1) {
-        int user_moved = 0;
-        while (!user_moved) {
-            switch (user_turn()) {
-            case -1:
-                cout << "Game break!" << endl;;
-                return 0;
-                break;
-            case 1:
-                cout << "Incorrect move!" << endl;;
-                break;
-            case 0:
-                user_moved = 1;
+        if (stroke % 2 + 1 == user_stroke)
+            if (user_handler() == END_GAME){
+                cout << "Game was ended premature" << endl;
                 break;
             }
+        else{
+            string ct = comp_turn();
+            cout << ct << endl;
         }
 
-        if (game_stop()) {
-            cout << "Game over! User win's!" << endl;;
-            break;
-        }
+        if (game_stop()){
+            cout << "Game over! " << (
+                stroke % 2 + 1 == user_stroke ? 
+                "User win's!" : 
+                "Super-advanced artificial intelligence win's!"
+            ) << endl;
 
-        string ct = comp_turn();
-
-
-        if (game_stop()) {
-            cout << "Game over! Super-advanced artificial intelligence win's!" << endl;;
             break;
         }
         
-        
-        cout << ct << '\n';
         show_game_state();
+
+        stroke++;
     }
+
 }
